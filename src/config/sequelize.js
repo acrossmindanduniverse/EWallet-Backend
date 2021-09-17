029dd1f7bb3e25010bf42b8c4840e4b473ac8a35
accrossmindanduniverse <fajarmccartney@gmail.com>
2021-09-17 21:31:10 +0700

    debugging auth controller

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,5 +1,14 @@
 const Sequelize = require('sequelize')
 
+const {
+  DB_HOST_DEV,
+  DB_NAME_DEV,
+  DB_USER_DEV,
+  DB_PASSWORD_DEV,
+  JWT_SECRET_KEY_DEV,
+  JWT_REFRESH_SECRET_DEV
+} = process.env
+
 // const {
 //   DB_HOST,
 //   DB_NAME,
@@ -9,23 +18,14 @@ const Sequelize = require('sequelize')
 //   JWT_REFRESH_SECRET
 // } = process.env
 
-const {
-  DB_HOST,
-  DB_NAME,
-  DB_USER,
-  DB_PASSWORD,
-  JWT_SECRET_KEY,
-  JWT_REFRESH_SECRET
-} = process.env
-
 const sequelize = {
-  database: new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    host: DB_HOST,
+  database: new Sequelize(DB_NAME_DEV, DB_USER_DEV, DB_PASSWORD_DEV, {
+    host: DB_HOST_DEV,
     dialect: 'mysql'
   }),
   JWT: {
-    secretKey: JWT_SECRET_KEY,
-    refreshSecretKey: JWT_REFRESH_SECRET
+    secretKey: JWT_SECRET_KEY_DEV,
+    refreshSecretKey: JWT_REFRESH_SECRET_DEV
   }
 }
 
